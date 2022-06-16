e420eebc65f9594f9a9f6a02cec29ab1a594c2a8
ChristianRosa518 <chrisrosa815518@gmail.com>
2022-06-16 17:56:06 -0400

    test7

--- server.js
+++ server.js
@@ -1,20 +1,19 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const stripe = require('stripe')(
-  'sk_test_51L273aBV3Tp8xOImETPF6Zrbjjje3GcmZeVOAc7C2CDBEaFxEAVfNhCKbwJXYchTnLaP6xWllJVhS3pBoyP2CA8V00sLoYZEQj'
-);
+// const stripe = require('stripe')(
+//   'sk_test_51L273aBV3Tp8xOImETPF6Zrbjjje3GcmZeVOAc7C2CDBEaFxEAVfNhCKbwJXYchTnLaP6xWllJVhS3pBoyP2CA8V00sLoYZEQj'
+// );
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
 
-app.use(express.static('build'));
-app.get('*', (req, res) => {
-  req.sendFile(path.resolve(__dirname, 'build', 'index.html'));
-});
-
-// if (process.env.NODE_ENV === 'production') {
-// }
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('build'));
+  app.get('*', (req, res) => {
+    req.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+  });
+}
 
 const calculateOrderAmount = (items) => {
   if (items <= 1) {
@@ -26,21 +25,21 @@ const calculateOrderAmount = (items) => {
   return number;
 };
 
-app.post('/create-payment-intent', async (req, res) => {
-  const { items } = req.body;
+// app.post('/create-payment-intent', async (req, res) => {
+//   const { items } = req.body;
 
-  // Create a PaymentIntent with the order amount and currency
-  const paymentIntent = await stripe.paymentIntents.create({
-    amount: calculateOrderAmount(items),
-    currency: 'usd',
-    automatic_payment_methods: {
-      enabled: true,
-    },
-  });
+//   Create a PaymentIntent with the order amount and currency
+//   const paymentIntent = await stripe.paymentIntents.create({
+//     amount: calculateOrderAmount(items),
+//     currency: 'usd',
+//     automatic_payment_methods: {
+//       enabled: true,
+//     },
+//   });
 
-  res.send({
-    clientSecret: paymentIntent.client_secret,
-  });
-});
+//   res.send({
+//     clientSecret: paymentIntent.client_secret,
+//   });
+// });
 
 app.listen(port, () => console.log('Node server listening on port 4242!'));
