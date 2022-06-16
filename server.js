7011accadcb40d1035e1d5728e4535c5f6ba5d1b
ChristianRosa518 <chrisrosa815518@gmail.com>
2022-06-16 17:31:10 -0400

    test3

--- server.js
+++ server.js
@@ -1,25 +1,20 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-
 const stripe = require('stripe')(
   'sk_test_51L273aBV3Tp8xOImETPF6Zrbjjje3GcmZeVOAc7C2CDBEaFxEAVfNhCKbwJXYchTnLaP6xWllJVhS3pBoyP2CA8V00sLoYZEQj'
 );
 
 app.use(express.json());
-app.use('/static', express.static(path.join(__dirname, 'client/build')));
 
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static('build'));
-//   app.get('*', (req, res) => {
-//     req.sendFile(path.resolve(__dirname, 'build', 'index.html'));
-//   });
-// }
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('build'));
+  app.get('*', (req, res) => {
+    req.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+  });
+}
 
 const calculateOrderAmount = (items) => {
-  // Replace this constant with a calculation of the order's amount
-  // Calculate the order total on the server to prevent
-  // people from directly manipulating the amount on the client
   if (items <= 1) {
     var number = 100;
   } else {
