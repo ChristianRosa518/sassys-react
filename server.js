65b95317932b3ac91b3fd16056e097e9695a193c
ChristianRosa518 <chrisrosa815518@gmail.com>
2022-07-03 19:13:54 -0400

    lelol

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const stripe = require('stripe')(stripeSecret);
 app.use(express.json());
 
 if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('build'));
   app.use('*', express.static('build'));
   app.get('*', (req, res) => {
     req.sendFile(path.resolve(__dirname, 'build', 'index.html'));
