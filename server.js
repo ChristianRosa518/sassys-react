617da853c11f44dff8ea8e77425193c5366f665d
ChristianRosa518 <chrisrosa815518@gmail.com>
2022-07-03 19:16:50 -0400

    lelol

--- server.js
+++ server.js
@@ -12,7 +12,6 @@ app.use(express.json());
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('build'));
-  app.use('*', express.static('build'));
   app.get('*', (req, res) => {
     req.sendFile(path.resolve(__dirname, 'build', 'index.html'));
   });
