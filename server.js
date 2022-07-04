8440e975676ca3d737e4cce767fbb25857d562d3
ChristianRosa518 <chrisrosa815518@gmail.com>
2022-07-03 23:15:21 -0400

    test

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const port = process.env.PORT || 5000;
 const express = require('express');
-const app = express();
 const path = require('path');
+const app = express();
 require('dotenv').config();
 
 // Keys
@@ -13,7 +13,7 @@ app.use(express.json());
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('build'));
   app.get('*', (req, res) => {
-    req.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+    req.sendFile(path.resolve(__dirname, 'build'));
   });
 }
 
